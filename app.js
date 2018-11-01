a69e957bd911b011004dfd17af33576f0ce82ccf
nerdylegend <31123377+nerdylegend@users.noreply.github.com>
2018-11-01 16:33:31 +0530

    Course 3.3

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-console.log('Starting App....');
+console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-const user = os.userInfo();
-
+console.log('Result :' + notes.add(9,-5));
 //fs.appendFileSync('greeting.txt','Hello World!');
-fs.appendFile('greeting.txt',`Hello ${user.username}!`, (err) => {
-  if (err) throw err;
-  console.log('File Operation complete');
-});
-console.log('Closing App...');
+// fs.appendFile('greeting.txt',`Hello ${user.username}!. You are ${notes.age}.`, (err) => {
+//   if (err) throw err;
+//   console.log('File Operation complete');
+// });
+// console.log('Closing App...');
