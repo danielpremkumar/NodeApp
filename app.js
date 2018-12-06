8885708afbe0a8692cb4b39073d7e7e727185640
nerdylegend <31123377+nerdylegend@users.noreply.github.com>
2018-12-06 07:21:04 +0530

    CODE CLEAN UP

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 console.log('Starting app.js');
+
 const fs = require('fs');
 const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
 console.log('Result :' + notes.add(9,-5));
 console.log(_.isString(notes.add(9,-5)));
-console.log(_.uniq([2,2,3,2,3,'Daniel','Daniel']));
+console.log(_.uniq([2,2,3,2,3,'Daniel']));
 //fs.appendFileSync('greeting.txt','Hello World!');
 // fs.appendFile('greeting.txt',`Hello ${user.username}!. You are ${notes.age}.`, (err) => {
 //   if (err) throw err;
